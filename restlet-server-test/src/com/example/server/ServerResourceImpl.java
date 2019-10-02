41c28fea4d4648030ed9b09379741b54ba4b4a9b
pjw <mathewpark90@gmail.com>
2019-10-02 13:26:42 +0900

    test

--- restlet-server-test/src/com/example/server/ServerResourceImpl.java
+++ restlet-server-test/src/com/example/server/ServerResourceImpl.java
@@ -2,24 +2,51 @@ package com.example.server;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.restlet.data.Form;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+//@Api
 public class ServerResourceImpl extends ServerResource {
 	Logger log4j = Logger.getLogger(ServerResourceImpl.class);
 	
 	@Get
 	public Representation webGet() {
-		log4j.debug("called.");
+		log4j.debug("@GET Method called.");
 		
 		JSONObject root = new JSONObject();
 		
-		root.put("msg", "test source.");
+		root.put("msg", "@GET Method called.");
+		
+		return new JsonRepresentation(root);
+	}
+	
+	@Post
+	public Representation webPost() {
+		Representation rep = getRequestEntity();
+		
+		JSONObject data = null;
+		
+		try {
+			if (rep.isAvailable()) {
+				Form form = new Form(rep);
+				
+				data = new JSONObject(java.net.URLDecoder.decode(form.getQueryString(), "UTF-8"));
+			}
+		} catch (Exception e) { }
+		
+		log4j.debug("@POST Method called.");
+		log4j.debug(String.format("body data=%s", data == null ? "null" : data.toString()));
+		
+		JSONObject root = new JSONObject();
+		
+		root.put("msg", "@POST Method called.");
 		
 		return new JsonRepresentation(root);
 	}
---
+++ restlet-server-test/src/com/example/server/Swagger2ApplicationImpl.java
@@ -0,0 +1,25 @@
+package com.example.server;
+
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.ext.swagger.Swagger2SpecificationRestlet;
+import org.restlet.routing.Router;
+
+public class Swagger2ApplicationImpl extends Application {
+    
+	public Restlet createInboundRoot() {
+		// Create a router Restlet that defines routes.
+		Router router = new Router(getContext());
+		
+		router.attach("admin/pings", ServerResourceImpl.class);
+
+	    // Configuring Swagger 2 support
+	    Swagger2SpecificationRestlet swagger2SpecificationRestlet = new Swagger2SpecificationRestlet(this);
+	    swagger2SpecificationRestlet.setBasePath("http://localhost:8889/");
+	    swagger2SpecificationRestlet.attach(router);
+	    
+	    router.attach(swagger2SpecificationRestlet);
+
+	    return router;
+	}
+}
